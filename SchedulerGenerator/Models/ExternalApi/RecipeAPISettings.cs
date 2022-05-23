b3e1ec8c69cadccd945680f9580df4ee072b7193
Kostiantyn Komkov <kkomkov99@gmail.com>
2022-05-23 16:53:52 +0100

    add dockerfile and docker compose project

--- SchedulerGenerator/Models/ExternalApi/RecipeAPISettings.cs
+++ SchedulerGenerator/Models/ExternalApi/RecipeAPISettings.cs
@@ -5,5 +5,10 @@
         public bool UseHttps { get; set; } = true;
         public string Host { get; set; }
         public int Port { get; set; }
+
+        public override string ToString()
+        {
+            return $" UseHttps:{UseHttps}, Host:{Host},Port:{Port}";
+        }
     }
 }
